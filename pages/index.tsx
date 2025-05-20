259db9bdb44326db19f0c4f4397dea86819f6d20
Dor00 <dorian.garcia@udea.edu.co>
2025-05-20 12:16:17 -0500

    github actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
      
       <div className="flex items-center justify-center h-screen">
-          <h1>hola mundo desde innosistemas...!</h1>
+          <h1>¡hola mundo desde innosistemas...!</h1>
       </div>
     
   );
